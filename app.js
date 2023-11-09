46fdde79ea3b3d094ed93725473a0ef59064564d
AgamalARM <agamalarm@gmail.com>
2023-11-09 15:23:57 +0200

    error Handler

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv').config();
 const morgan = require("morgan");
 const dbConnect = require('./config/dbConnect');
 const authRouter = require('./routes/authRoute');
+const notFound = require('./middlewares/errorHandler');
+const errorHandler = require('./middlewares/errorHandler');
 // db connection
 dbConnect();
 
@@ -13,6 +15,10 @@ app.use(morgan("dev"));  // morgan middelware for logger
 const port = process.env.PORT || 3000 ;
 
 app.use('/api/user',authRouter);
+app.use(notFound);
+app.use(errorHandler);
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,16 @@
 // Models 
 const User = require('../models/userModel');
+//Error Handler
+const asyncHandler = require('express-async-handler');
 
 
 
 //functions
-const createUser = async (req,res) => {
+const createUser = asyncHandler(async (req,res) => {
   const email = req.body.email;
   const findUser = await User.findOne({email: email});
   if (!findUser) {
+    // Create a new user
     const newUser = await User.create(req.body);
     res.json(newUser);
     
@@ -18,9 +21,7 @@ const createUser = async (req,res) => {
     });
     
   }
-
-
-};
+});
 
 
 // exports modules
